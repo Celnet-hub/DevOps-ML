92109ad586d887892b28385105e4229fcd62a5a8
chidubem <nwabuisicelestine@gmail.com>
2022-08-23 21:07:00 +0000

    created a k8 cluster and pods to run container images

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=celnet1/app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy mypython-app --image=$dockerpath:myfirstag
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/mypython-app-5fcd98698c-6lv58 --address 0.0.0.0 8000:80
